a2a62388be3b5194547be892b159b00ad9493399
anderrf <you@example.comandersonrochafarias@gmail.com>
2019-11-11 10:11:18 -0300

    percebida falha na alteração, imagem some com nova mudança

--- php/cliente/alterarCliente.php
+++ php/cliente/alterarCliente.php
@@ -13,7 +13,10 @@
     $telefone = $_POST['telefone'];
     $celular = $_POST['celular'];
     $email = $_POST['email'];
-    $foto = $_POST['foto'];
+    if($_POST['foto'] != ''){
+      $foto = $_POST['foto'];
+    }
+    
 
     $query = "UPDATE tb_Cliente SET nm_endereco = '$endereco', nr_numeroEndereco = '$numero', nm_bairro = '$bairro', nm_cidade = '$cidade', nm_UF = '$UF', nm_referencia = '$referencia', nr_telefone = '$telefone', nr_celular = '$celular', ds_email = '$email' WHERE cd_cliente = '$codCliente';";
 
